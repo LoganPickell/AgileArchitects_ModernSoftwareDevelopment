27f0035c7d4e2f6af90303d37920ab55a1f973fe
LoganPickell <lbpickell@ualr.edu>
2025-04-27 12:17:33 -0500

    Attempt to fix CI

--- app_testing_files/conftest.py
+++ app_testing_files/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from app import create_app, db
+import os
 
 
 @pytest.fixture
@@ -7,7 +8,8 @@ def client():
     app = create_app({
         'TESTING': True,
         'SQLALCHEMY_DATABASE_URI': 'sqlite:///:memory:',
-        'SQLALCHEMY_TRACK_MODIFICATIONS': False
+        'SQLALCHEMY_TRACK_MODIFICATIONS': False,
+        'template_folder': os.path.join(os.path.dirname(__file__), 'app', 'templates'),
     })
 
     with app.test_client() as client:
